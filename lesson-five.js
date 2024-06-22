e7b304d662ff3b53f00d326b5549bb6ad027d028
jason beedle <jasonbeedle@jasons-mbp.mynet>
2024-06-22 20:11:24 +0100

    lesson-five

---
+++ lesson-five.js
@@ -0,0 +1,5 @@
+// circle: Area = 𝜋 × radius 2 Area=π×radius 2
+function calculateCircleArea(radius) {
+    return Math.PI * Math.pow(radius, 2);
+}
+console.log(calculateCircleArea(5)); // Should print: 78.53981633974483 (approximately)
---
+++ lesson-five.ts
@@ -0,0 +1,7 @@
+// circle: Area = 𝜋 × radius 2 Area=π×radius 2
+function calculateCircleArea(radius: number): number {
+    return Math.PI * Math.pow(radius, 2);
+}
+  
+  console.log(calculateCircleArea(5)); // Should print: 78.53981633974483 (approximately)
+  
